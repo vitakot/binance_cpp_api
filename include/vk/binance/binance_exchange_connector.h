d9297dea6e62d0a87ee42d135b7bef75223d236f
Vitezslav Kot <vitezslav.kot@stonky.cz>
2025-03-09 01:10:42 +0100

    Fix win build

--- include/vk/binance/binance_exchange_connector.h
+++ include/vk/binance/binance_exchange_connector.h
@@ -13,7 +13,7 @@ Copyright (c) 2022 Vitezslav Kot <vitezslav.kot@gmail.com>.
 #include "vk/interface/i_exchange_connector.h"
 #include "vk/common/module_factory.h"
 #include <memory>
-#include <magic_enum.hpp>
+#include <magic_enum/magic_enum.hpp>
 
 namespace vk {
 class BinanceFuturesExchangeConnector final : public IExchangeConnector {
