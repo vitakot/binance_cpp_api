565bbfb75f9f6b5958639f0f85c0a540f197c15d
Vitezslav Kot <vitezslav.kot@gmail.com>
2025-11-08 06:42:09 +0100

    Remove problematic includes

--- src/binance_futures_ws_client.cpp
+++ src/binance_futures_ws_client.cpp
@@ -10,7 +10,6 @@ Copyright (c) 2022 Vitezslav Kot <vitezslav.kot@gmail.com>.
 #include "vk/utils/json_utils.h"
 #include <boost/beast/core.hpp>
 #include <boost/algorithm/string/case_conv.hpp>
-#include <fmt/format.h>
 #include <thread>
 
 using namespace std::chrono_literals;
